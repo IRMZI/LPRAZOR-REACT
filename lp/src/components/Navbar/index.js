0006577c67e1cdb83a361ebc1c77c066d0d9f7f5
Rafael Romariz <94732569+IRMZI@users.noreply.github.com>
2022-10-25 11:23:41 -0300

    Criando navbar
    Adicionando styledcomponents

---
+++ lp/src/components/Navbar/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavItem } from 'reactstrap';
+import {Nav, NavbarContainer, Navlogo} from './NavbarElements';
+const Navbar = () => {
+  return (
+    <>
+    <Nav>
+    <NavbarContainer>
+    <Navlogo to = '/'>
+        RAZOR
+    </Navlogo>
+    <MobileIcon>
+    <FaBars/>
+    </MobileIcon>
+    <NavMenu>
+        <NavItem>
+            <NavLinks to='about'> about </NavLinks>
+        </NavItem>
+    </NavMenu>
+    </NavbarContainer>
+    </Nav>
+    
+    </>
+  )
+}
+
+export default Navbar;
