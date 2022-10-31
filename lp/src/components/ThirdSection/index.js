adb3d18188ee70bcaa2f5d400a5ecbebbbbff7fc
Rafael Romariz <94732569+IRMZI@users.noreply.github.com>
2022-10-31 11:06:25 -0300

    ultima mudança

--- lp/src/components/ThirdSection/index.js
+++ lp/src/components/ThirdSection/index.js
@@ -9,7 +9,7 @@ const ThirdSection = () => {
         <div>
             <a href='https://razor.com.br/sobre/'> <img className="Crazor" src={CRazor} alt=""/>  </a>
             <img className="donosdaRazor" src={donos} alt="DonosdaRazor"/> 
-            <h1 className="tituloroxo">PARA OS PROFISSIONAIS MAIS EXIGENTES</h1>
+            <h1 id="OHW">PARA OS PROFISSIONAIS MAIS EXIGENTES</h1>
         </div>
         <div>
             <h1 className="titulobranco">QUE USAM OS SOFTWARES MAIS EXIGENTES</h1>
